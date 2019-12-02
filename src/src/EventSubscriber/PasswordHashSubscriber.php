2ec869a24569e7d2c32c545141a06793ae77437b
alexandremajda <majda.a@outlook.com>
2019-12-02 11:59:58 +0100

    [Feature] Adding the event which hash the password on registration, more infos in class comments

---
+++ src/src/EventSubscriber/PasswordHashSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class PasswordHashSubscriber implements EventSubscriberInterface {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    public static function getSubscribedEvents(){
+        
+        // Define a priority in the kernel on the function defined below
+        return [
+            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+
+    // The "GetResponseForControllerResultEvent" has been renamed as "ViewEvent"
+    public function hashPassword(ViewEvent $event) {
+
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || Request::METHOD_POST !== $method)
+            return;
+
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $user->getPassword())
+        );
+    }
+}
+
+?>
