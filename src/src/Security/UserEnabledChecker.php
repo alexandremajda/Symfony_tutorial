bf3c5a1a14b4361304c476b518020b6dcacfe5e5
alexandremajda <majda.a@outlook.com>
2019-12-04 14:30:54 +0100

    [Doc] adding a descriptiv doc

--- src/src/Security/UserEnabledChecker.php
+++ src/src/Security/UserEnabledChecker.php
@@ -7,6 +7,10 @@ use Symfony\Component\Security\Core\Exception\DisabledException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 
+/**
+ * A simple class that check if the user account is enable
+ * throws DisabledException if not
+ */
 class UserEnabledChecker implements UserCheckerInterface
 {
     public function checkPreAuth(UserInterface $user)
