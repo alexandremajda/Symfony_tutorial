487e57998669cfab3e12e5f03be82c3396d643cd
alexandremajda <majda.a@outlook.com>
2019-12-05 16:31:45 +0100

    [Adding] Image entity to the Db

---
+++ src/src/Entity/Image.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity()
+ * @Vich\Uploadable()
+ * @ApiResource(
+ * )
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Specify a specific mapping defined by its name in the related configuration file
+     * File Name Property symbolise the url where users can see uploaded files
+     * @Vich\UploadableField(mapping="images", fileNameProperty="url")
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @var [type]
+     */
+    private $url;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+}
