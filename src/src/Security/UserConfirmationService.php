7dbccccc44b71bffab875a6855f48ca2ebd282a9
alexandremajda <majda.a@outlook.com>
2019-12-08 15:07:08 +0100

    [Feature] adding some logs

--- src/src/Security/UserConfirmationService.php
+++ src/src/Security/UserConfirmationService.php
@@ -5,30 +5,39 @@ namespace App\Security;
 use App\Exception\InvalidConfirmationTokenException;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserConfirmationService
 {
     private $userRepository;
     private $entityManager;
+    private $logger;
 
     public function __construct(
         UserRepository $userRepository,
-        EntityManagerInterface $entityManager)
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger)
     {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function confirmUser(string $confirmationToken)
     {
+        $this->logger->debug('Fetching user by confirmation token');
         $user = $this->userRepository->findOneBy(['confirmationToken' => $confirmationToken]);
         
-        if (!$user)
+        if (!$user) {
+            $this->logger->debug('User confirmation token not found');            
             throw new InvalidConfirmationTokenException();
+        }
             
         $user->setEnable(true);
         $user->setConfirmationToken(null);
         $this->entityManager->flush();
+
+        $this->logger->debug('Confirmed user by confirmation token');
     }
 }
